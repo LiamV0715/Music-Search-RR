aeceba5efc3985943d5282ae619fc503f29fb8b9
Liam Vermette <Liam.vermette@gmail.com>
2024-05-22 20:30:02 -0400

    added search functionality and gallery and huge debug

--- src/App.js
+++ src/App.js
@@ -1,33 +1,84 @@
-import {useEffect, useState } from 'react'
+import {useEffect, useRef, useState } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/Searchbar'
 import { useContext } from 'react'
 import { SongContext } from './components/SongContext'
+import { SearchContext} from './components/SearchContext'
 
 function App() {
-	let [search, setSearch] = useState('')
-	let [message, setMessage] = useState('Search for Music!')
-	let [songData, setSongData] = useState([])
+	let [searchTerm, setSearchTerm] = useState('');
 
-  
-	
+  let [message, setMessage] = useState("Search for Music!");
 
-	
+  let [songData, setSongData] = useState([]);
+
+  let searchInput = useRef('')
+
+ 
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+
+      document.title = `${searchTerm} Music`;
+
+      const response = await fetch('https://itunes.apple.com/search?term=the%20grateful%20dead');
+
+      const resData = await response.json();
+
+      if (resData.results.length > 0) {
+
+        setSongData(resData.results);
+
+      } else {
+
+        setMessage('Not Found');
+
+      }
+
+      console.log(resData);
+
+    }
+
+    fetchData();
+
+  }, [searchTerm]);
 	
 	const handleSearch = (e, term) => {
-		e.preventDefault()
-		setSearch(term)
-	}
 
-	return (
-		<div>
-			<SearchBar handleSearch = {handleSearch}/>
-			{message}
-			<SongContext.Provider value={songData}>
-				<Gallery />
-			</SongContext.Provider>
-		</div>
-  	);
+    e.preventDefault()
+
+    setSearchTerm(term)
+
+  }
+
+  return (
+
+    <div className="App">
+
+      <SearchContext.Provider value={{
+
+        term: searchInput,
+
+        handleSearch: handleSearch
+
+      }} >
+
+        <SearchBar />
+
+      </SearchContext.Provider>
+
+      {message}
+
+      <SongContext.Provider value={songData}>
+
+        <Gallery />
+
+      </SongContext.Provider>
+
+    </div>
+
+  );
 }
 
 export default App;
---
+++ src/components/Searchbar.jsx
@@ -0,0 +1,16 @@
+// SeachBar.js
+import { useContext } from 'react'
+import { SearchContext } from './SearchContext'
+
+function SearchBar() {
+    const {term, handleSearch} = useContext(SearchContext)
+
+    return (
+        <form>
+            <input ref={term} type="text" placeholder="Search Here" />
+            <button onClick={(e) => handleSearch(e, term)}>Submit</button>
+        </form>
+    )
+}
+
+export default SearchBar
