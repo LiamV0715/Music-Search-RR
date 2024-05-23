fe087e52b78a632d434de6e6ddbb9886d26ea1ca
Liam Vermette <Liam.vermette@gmail.com>
2024-05-22 20:51:53 -0400

    took away search defaults and added css link

--- src/App.js
+++ src/App.js
@@ -1,45 +1,60 @@
 import { useEffect, useRef, useState } from "react";
 import Gallery from "./components/Gallery";
 import SearchBar from "./components/Searchbar";
+import './App.css';
 import { SongContext } from "./components/SongContext";
 import { SearchContext } from "./components/SearchContext";
 
 function App() {
-  let [searchTerm, setSearchTerm] = useState("");
-  let [message, setMessage] = useState("Search for Music!");
-  let [songData, setSongData] = useState([]);
-  let searchInput = useRef("");
 
-  useEffect(() => {
-    const fetchData = async () => {
-      document.title = `${searchTerm} Music`;
-      const response = await fetch(
-        "https://itunes.apple.com/search?term=the%20grateful%20dead"
-      );
-      const resData = await response.json();
-      if (resData.results.length > 0) {
-        setSongData(resData.results);
-      } else {
-        setMessage("Not Found");
-      }
-      console.log(resData);
-    };
-    fetchData();
-  }, [searchTerm]);
+  let [message, setMessage] = useState('Search for Music!')
+
+  let [songData, setSongData] = useState([])
+
+  let searchInput = useRef('')
+
+ 
+
+  const API_URL = 'https://itunes.apple.com/search?term='
+
+ 
 
   const handleSearch = (e, term) => {
-    e.preventDefault();
-    setSearchTerm(term);
-  };
+
+    e.preventDefault()
+
+    // Fetch Data
+
+    const fetchData = async () => {
+
+      document.title = `${term} Music`
+
+      const response = await fetch(API_URL + term)
+
+      const resData = await response.json()
+
+      if (resData.results.length > 0) {
+
+        // Set State and Context value
+
+        return setSongData(resData.results)
+
+      } else {
+
+        return setMessage('Not Found')
+
+      }
+    }
+    fetchData()
+
+  }
 
   return (
     <div className="App">
-      <SearchContext.Provider
-        value={{
-          term: searchInput,
-          handleSearch: handleSearch,
-        }}
-      >
+      <SearchContext.Provider value={{
+        term: searchInput,
+        handleSearch: handleSearch
+      }}>
         <SearchBar />
       </SearchContext.Provider>
       {message}
